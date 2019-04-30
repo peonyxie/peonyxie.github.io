649f6bae275dfc44b1d5c98931c97a8cd1132458
Yuhan Xie <yuhan@berkeley.edu>
2019-04-29 19:57:29 -0700

    whole project

---
+++ final proj/assets/js/next.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('#button').click(function(){
+		$("body").css("background-image","none");
+		$("body").css("background-color","#EDC7B7");
+		$('#button').css("display","none");
+		$('#click').css("display","none");
+		$("#night").css('display','none');
+		$('#drawing').css('display', 'block');
+		$('#gift').css('display','block');
+		$('#birthday').css('display','block');
+	})
+})
+
+
+
